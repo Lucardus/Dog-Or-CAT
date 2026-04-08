a8feee9e9e2faa2417e64392addd007aac189da6
Lucardus <lucaseduardoorlandoleo11@gmail.com>
2026-04-08 11:37:20 -0300

    Error Line 3 Fix2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 __all__ = ['is_cat', 'learn', 'classify_image', 'categories', 'image', 'label', 'examples', 'intf']
 
 from fastai.vision.all import *
+from fastai import *
 import gradio as gr
 
 def is_cat(x): return x[0].isupper() 
@@ -18,4 +19,4 @@ label = gr.outputs.Label()
 examples = ['dog.jpg', 'cat.jpg', 'dunno.jpg']
 
 intf = gr.Interface(fn=classify_image, input=image, outputs=label, examples=examples)
-intf.launch(inline=False) 
+intf.launch(inline=False)  
