912e94ad2b078723212f322224f9ce4e00aaa99f
Lucardus <lucaseduardoorlandoleo11@gmail.com>
2026-04-08 13:41:11 -0300

    fix gradio syntax

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 __all__ = ['is_cat', 'learn', 'classify_image', 'categories', 'image', 'label', 'examples', 'intf']
 
+from fastai.vision.all import *
 from fastai.basics import *
 import gradio as gr
 
-def is_cat(x): return x[0].isupper() 
+def is_cat(x): return x[0].isupper()
 
 learn = load_learner('model.pkl')
 
-categories = ('Dog','Cat')
+categories = ('Dog', 'Cat')
 
 def classify_image(img):
     pred,idx,probs = learn.predict(img)
-    return dict(zip(categories, map(float,probs)))
+    return dict(zip(categories, map(float, probs)))
 
-image = gr.inputs.Image(shapes=(192, 192))
-label = gr.outputs.Label()
-examples = ['dog.jpg', 'cat.jpg', 'dunno.jpg']
-
-intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
-intf.launch(inline=False)
+intf = gr.Interface(
+    fn=classify_image,
+    inputs=gr.Image(width=192, height=192),
+    outputs=gr.Label(),
+    examples=[]
+)
+intf.launch()
